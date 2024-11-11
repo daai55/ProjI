393865e401fffe5d31d4c38e30c50c475d2268a4
daai55 <daaine.souza2003@gmail.com>
2024-11-11 18:36:17 -0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ with st.form(key="form_agendamento"):
             }
 
             # Substitua a URL abaixo pela URL real do backend caso tenha um.
-            url_backend = "http://localhost:5000/agendamentos"
+            url_backend = "https://fpghdtrd5gtaxkph8kowdm.streamlit.app/"
             response = requests.post(url_backend, json=dados)
 
             if response.status_code == 200:
